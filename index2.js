e19d946059620c6cd7b958b9c00376be0767365a
houssinCarnelian <houssin.carnelian@gmail.com>
2024-04-08 01:28:31 +0100

    working with token

--- index2.js
+++ index2.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./config/dbConnect.js");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const app = express();
 const PORT = process.env.PORT || 4000;
 dbConnect();
@@ -10,9 +12,22 @@ const sendMail = require("./sendEmails.js");
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.post("/token", async (req, res) => {
+  const { uertokn } = req.body;
+  let userOneToken = "firstuserToken";
+  const salt = await bcrypt.genSaltSync(10);
+  userOneToken = await bcrypt.hashSync(userOneToken, salt);
+  const theCompairedToken = await bcrypt.compare(uertokn, userOneToken);
+  if (theCompairedToken) {
+    res.json({ message: "you are authorized ", userOneToken });
+  } else {
+    res.json({ message: "you are not authorized please check your token" });
+  }
+});
+
 app.get("/tn", (req, res) => {
-  const user = false;
-  const admin = true;
+  const user = true;
+  const admin = false;
   if (user) {
     return res.redirect("/us");
   }
