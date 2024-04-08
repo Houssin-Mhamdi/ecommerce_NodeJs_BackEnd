4636c5d494a3b33e9ea8944c0b81706b1d841092
houssinCarnelian <houssin.carnelian@gmail.com>
2024-04-09 00:19:35 +0100

    adding tokens to the user

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken.js");
 const User = require("../models/userModle.js");
 const asyncHandler = require("express-async-handler");
 
@@ -16,7 +17,16 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const findUser = await User.findOne({ email });
   if (findUser && (await findUser.isPasswordMatched(password))) {
-    res.json({ message: "login sccessful", findUser });
+    res.json({
+      message: "login sccessful",
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      role: findUser?.role,
+      token: generateToken(findUser?._id),
+    });
   } else {
     throw new Error("user not found");
   }
