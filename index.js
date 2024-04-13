da2967d50513410310553cdbaa6b30b016ee11d7
houssinCarnelian <houssin.carnelian@gmail.com>
2024-04-13 15:06:53 +0100

    creating the ProductModle

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const morgan = require("morgan");
 const connectToDB = require("./config/dbConnect.js");
 const bodyParser = require("body-parser");
 const authRoute = require("./routes/authRoute.js");
@@ -9,12 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 connectToDB();
 
-
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
 
 app.use("/api/user", authRoute);
 
