8c68adc404ef18586382be35f3af32d69925ca8a
houssinCarnelian <houssin.carnelian@gmail.com>
2024-05-05 17:40:10 +0100

    install mlter sharp and add cloudinary files

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary");
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -6,3 +6,37 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUD_API_SECRET,
   secure: true,
 });
+const cloudinaryUploadImg = async (fileToUploads) => {
+  try {
+    const result = await cloudinary.v2.uploader.upload(fileToUploads, {
+      resource_type: "auto",
+    });
+    return {
+      url: result.secure_url,
+      asset_id: result.asset_id,
+      public_id: result.public_id,
+    };
+  } catch (error) {
+    // Handle any errors
+    console.error("Error uploading image to Cloudinary:", error);
+    throw error;
+  }
+};
+const cloudinaryDeleteImg = async (fileToDelete) => {
+  try {
+    const result = await cloudinary.v2.uploader.destroy(fileToDelete, {
+      resource_type: "auto",
+    });
+    return {
+      url: result.secure_url,
+      asset_id: result.asset_id,
+      public_id: result.public_id,
+    };
+  } catch (error) {
+    // Handle any errors
+    console.error("Error uploading image to Cloudinary:", error);
+    throw error;
+  }
+};
+
+module.exports = { cloudinaryUploadImg, cloudinaryDeleteImg };
